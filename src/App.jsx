bb8eee3f4d3fa82c554677be8ad30849290f561f
Abdul Sabit Ariff <58955721+hadid25@users.noreply.github.com>
2023-11-13 06:55:36 +0000

    Created Blocking Message for Mobile Screens

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import Header from "./components/Header";
 
 const App = () => {
-
-  return(
-    <div className="App">
-      <Header/>
-    </div>
-  )
+  if(window.innerWidth < 1000){
+    return(
+        <div className="Mobile">
+          Mobile Devices Not Supported
+        </div>
+    )
+  }else{
+    return(
+      <div className="App">
+        <Header/>
+      </div>
+    )
+  }
 }
 
 export default App;
