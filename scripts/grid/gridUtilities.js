5c7a20d536902b76b811571f39ab22a1f919ae29
Abdul Sabit Ariff <58955721+hadid25@users.noreply.github.com>
2023-11-15 17:21:13 +0000

    Refactoring functions to read from worldGridParameters object

--- scripts/grid/gridUtilities.js
+++ scripts/grid/gridUtilities.js
@@ -1,5 +1,5 @@
 // Renders the world grid onto the DOM
-export function renderWorldGrid(rows, columns, gridSquareSize){
+export function renderWorldGrid(worldGridParameters){
   // Creating accessor for the grid container
   const worldGridContainer = document.getElementById('world-grid-container');
 
@@ -8,31 +8,31 @@ export function renderWorldGrid(rows, columns, gridSquareSize){
   worldGrid.setAttribute('id', 'world-grid');
   worldGridContainer.appendChild(worldGrid);
   
-
-  for (let row = 0; row < rows; row++ ){
-    for (let column = 0; column < columns; column++ ){
+  // Rendering world grid to DOM
+  for (let row = 0; row < worldGridParameters.rows; row++ ){
+    for (let column = 0; column < worldGridParameters.columns; column++ ){
       const gridItem = document.createElement('div');
       gridItem.setAttribute('class', 'grid-item')
       gridItem.setAttribute('id', `${row}-${column}`);
       gridItem.setAttribute('data-celltype', 'empty'); // set all cell types to 'empty' by default
-      gridItem.style.height = gridSquareSize;
-      gridItem.style.width = gridSquareSize;
+      gridItem.style.height = worldGridParameters.gridSquareSize;
+      gridItem.style.width = worldGridParameters.gridSquareSize;
       worldGrid.appendChild(gridItem);
-      worldGrid.style.gridTemplateColumns = (String(gridSquareSize) + "px ").repeat(columns)
+      worldGrid.style.gridTemplateColumns = (String(worldGridParameters.gridSquareSize) + "px ").repeat(worldGridParameters.columns)
     }
   }
 }
 
 
-export function readWorldGridState(rows, columns){
+export function readWorldGridState(worldGridParameters){
   const worldGrid = document.getElementById('world-grid')
   let currentWorldGridState = [];
 
-  for (let row = 0; row < rows; row++ ){
+  for (let row = 0; row < worldGridParameters.rows; row++ ){
     // Create a row with the required columns, filling with zeros by default
-    currentWorldGridState.push(Array(columns).fill(0));
+    currentWorldGridState.push(Array(worldGridParameters.columns).fill(0));
     // Index each cell in the newly created column and populate them with the DOM element's celltype value
-    for (let column = 0; column < columns; column++ ){
+    for (let column = 0; column < worldGridParameters.columns; column++ ){
       currentWorldGridState[row][column] = document.getElementById(`${row}-${column}`).dataset.celltype; //store just the cell type
     }
   }
--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,17 @@
 import blockMobile from './utilities/blockMobile.js'
 import {renderWorldGrid, readWorldGridState} from './grid/gridUtilities.js'
 
-const grid_parameters = {
+const worldGridParameters = {
   //do not exceed 20 rows and 54 columns
   'rows': 20,
   'columns': 54,
-  'gridSquareSize': 30 //px
+  'gridSquareSize': 30, //px
+  'default_start_cell': '10-20',
+  'default_goal_cell': '10-25'
 }
 
 //blockMobile(); // Block functionality on mobile devices
-let worldGridState = renderWorldGrid(grid_parameters.rows, grid_parameters.columns, grid_parameters.gridSquareSize); 
+let worldGridState = renderWorldGrid(worldGridParameters); 
 
 
-worldGridState = readWorldGridState(grid_parameters.rows, grid_parameters.columns)
+worldGridState = readWorldGridState(worldGridParameters)
