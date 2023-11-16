6f8ad28285f6e2113436034eb91dc044762ad19e
Abdul Sabit Ariff <58955721+hadid25@users.noreply.github.com>
2023-11-16 11:44:38 +0000

    Added comment for theme

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 import blockSmallScreens from './utilities/blockSmallScreens.js'
 import {renderWorldGrid, readWorldGridState} from './grid/gridUtilities.js'
-import './utilities/theme.js'
+import './utilities/theme.js' //to activate them toggling (this is enough to work since it executes on import)
 
 const worldGridParameters = {
   //do not exceed 20 rows and 54 columns
