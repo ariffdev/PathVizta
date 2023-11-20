6fd749b3ee975ef1722435e77eedabead65e8c9b
Abdul Sabit Ariff <58955721+hadid25@users.noreply.github.com>
2023-11-20 10:15:20 +0000

    Switched to simple transitions

--- scripts/visualisation/visualizationRunner.js
+++ scripts/visualisation/visualizationRunner.js
@@ -1,4 +1,4 @@
-import { renderWorldGrid } from "../grid/gridUtilities.js";
+
 
 export function visualizationRunner(algorithmOutput, inputData){
   let visualisationSpeed = inputData.visualisationSpeed;
@@ -10,8 +10,7 @@ export function visualizationRunner(algorithmOutput, inputData){
   // consider disabling obstacle addition and start and goal cell changes while visuzalization is running
 }
 
-
-
+//Simple transitions
 function visualizeSearchProcess(algorithmOutput, specialCells, visualisationSpeed){
     let start = specialCells.start;
     let goal = specialCells.goal;
@@ -20,6 +19,7 @@ function visualizeSearchProcess(algorithmOutput, specialCells, visualisationSpee
     let [row, column] = node;
     if((row !== start[0] || column !== start[1]) && (row !== goal[0] || column !== goal[1])){ // executing only for non-goal / non-start cells
       document.getElementById(`${row}-${column}`).style.backgroundColor = 'green';
+      document.getElementById(`${row}-${column}`).style.transition = 'all 1s';
     }    
   })
 }
@@ -33,6 +33,44 @@ function visualizeShortestPath(algorithmOutput, specialCells, visualisationSpeed
     let [row, column] = node;
     if ((row !== start[0] || column !== start[1]) && (row !== goal[0] || column !== goal[1])) { // executing only for non-goal / non-start cells
       document.getElementById(`${row}-${column}`).style.backgroundColor = 'yellow';
+      document.getElementById(`${row}-${column}`).style.transition = 'all 5s';
     }
   })
 }
+
+// Complex transitions to be returned to. Works in conjunction with visualizations.css
+// function visualizeSearchProcess(algorithmOutput, specialCells, visualisationSpeed){
+//     let start = specialCells.start;
+//     let goal = specialCells.goal;
+//   //Loop through search path and apply visualizing styles to them
+//   algorithmOutput.visitedCells.forEach((node) => {
+//     let [row, column] = node;
+//     if((row !== start[0] || column !== start[1]) && (row !== goal[0] || column !== goal[1])){ // executing only for non-goal / non-start cells
+//       // document.getElementById(`${row}-${column}`).classList.add('animated-visited-cell');
+//       // document.getElementById(`${row}-${column}`).style.animation = 'animatedVisitedCell 1s'
+//       document.getElementById(`${row}-${column}`).style.transition = 'scale(5)';
+//       // document.getElementById(`${row}-${column}`).style.backgroundColor = 'green';
+
+//     }    
+//   })
+// }
+
+
+// function visualizeShortestPath(algorithmOutput, specialCells, visualisationSpeed){
+//     let start = specialCells.start;
+//     let goal = specialCells.goal;
+//   //Loop through search path and apply visualizing styles to them
+//   algorithmOutput.shortestPath.forEach((node) => {
+//     let [row, column] = node;
+//     if ((row !== start[0] || column !== start[1]) && (row !== goal[0] || column !== goal[1])) { // executing only for non-goal / non-start cells
+//       document.getElementById(`${row}-${column}`).classList.add('animated-shortest-path-cell');
+//       // document.getElementById(`${row}-${column}`).style.transition = 'all 5s';
+//     }
+//   })
+// }
+
+
+
+
+
+// Consider using both transitions and animations
